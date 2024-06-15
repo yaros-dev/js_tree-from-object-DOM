3c84f442650a862c8e2ede54b71881415138357d
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-06-16 01:13:29 +0300

    make build

---
+++ index.abdbf81c.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n?n.append(r):(r.id="tree",document.body.append(r)),t){var c=document.createElement("li");c.textContent=a,r.append(c),Object.keys(t[a]).length&&e(c,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.abdbf81c.js.map
